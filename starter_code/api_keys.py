e55969208c999c8f5e7b7f1428c99b9cf1075b86
prisp90 <66030831+prisp90@users.noreply.github.com>
2022-04-18 18:05:20 -0700

    Add files via upload

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "6142a947f82a54e323b1582031a45d09"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBRcR9lzTXSOmggkXDFJWtxa_hCQdHrw34"
